97083cc053ae5737715a1deafcf7badacd1e8c55
Md Minhaj Uddin <104689719+ripon5098@users.noreply.github.com>
2023-09-30 20:52:25 +0600

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,8 @@ def predict_emotions(input_audio):
             # str(input_audio),
             # input_audio.filename
             abs_file_path,
-            ""
+            "",
+            api_name="/predict"
             # "/predict"
         )
 
